d5cc722fa37b5a4dc52ee6b2d0af2e21cb53a0b5
高小川 <gaoxiaochuan@hotmail.com>
2017-10-10 10:01:57 +0800

    add elasticsearch

--- src/Demeter.FileComponent/DemeterFileStore.cs
+++ src/Demeter.FileComponent/DemeterFileStore.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Demeter.FormComponent;
 using MongoDB.Driver;
+using Nest;
 
 namespace Demeter.FileComponent
 {
@@ -11,12 +12,14 @@ namespace Demeter.FileComponent
         where TFile : DemeterFile, new()
     {
         private readonly IMongoCollection<TFile> _formCollection;
+        private readonly ElasticClient _elasticClient;
         private readonly string _baseFolderPath;
         
         public DemeterFileStore(
             IMongoDatabase database,
             string formsCollection,
-            string folderPath)
+            string folderPath,
+            ElasticClient elasticClient = null)
         {
             if (database == null)
             {
@@ -28,37 +31,60 @@ namespace Demeter.FileComponent
             }
             this._formCollection = database.GetCollection<TFile>(formsCollection);
             this._baseFolderPath = folderPath;
+
+            this._elasticClient = elasticClient;
         }
 
-        async Task<FormResult> IFormStore<TFile>.CreateAsync(TFile form, CancellationToken cancellationToken)
+        async Task<FormResult> IFormStore<TFile>.CreateAsync(TFile file, CancellationToken cancellationToken)
         {
-            if (form == null)
+            if (file == null)
             {
-                throw new ArgumentNullException(nameof(form));
+                throw new ArgumentNullException(nameof(file));
             }
 
+            Mutex writeMutex = new Mutex(true);
             await Task.WhenAll(
-                DemeterFileUtil.WriteAsync(this._baseFolderPath, form.Id, form.Content),
+                Task.Run(async () =>
+                {
+                    writeMutex.WaitOne();
+                    await DemeterFileUtil.WriteAsync(this._baseFolderPath, file.Id, file.Content);
+                    writeMutex.ReleaseMutex();
+                }),
                 this._formCollection
-                .InsertOneAsync(form, cancellationToken: cancellationToken)
+                .InsertOneAsync(file, cancellationToken: cancellationToken),
+                Task.Run(async () =>
+                {
+                    if (this._elasticClient == null)
+                    {
+                        return ;
+                    }
+                    byte[] tempStore = file.Content;
+                    writeMutex.WaitOne();
+                    file.Content = null;
+                    await this._elasticClient.IndexAsync(file, m => m.Id(file.Id));
+                    file.Content = tempStore;
+                    writeMutex.ReleaseMutex();
+                })
             ).ConfigureAwait(false);
+
+            writeMutex.Dispose();
 
             return FormResult.Success;
         }
 
-        async Task<FormResult> IFormStore<TFile>.DeleteAsync(TFile form, CancellationToken cancellationToken)
+        async Task<FormResult> IFormStore<TFile>.DeleteAsync(TFile file, CancellationToken cancellationToken)
         {
-            if (form == null)
+            if (file == null)
             {
-                throw new ArgumentNullException(nameof(form));
+                throw new ArgumentNullException(nameof(file));
             }
-            form.Delete();
+            file.Delete();
 
             var query = Builders<TFile>.Filter.And(
-                Builders<TFile>.Filter.Eq(f => f.Id, form.Id),
+                Builders<TFile>.Filter.Eq(f => f.Id, file.Id),
                 Builders<TFile>.Filter.Eq(f => f.DeleteOn, null)
             );
-            var update = Builders<TFile>.Update.Set(f => f.DeleteOn, form.DeleteOn);
+            var update = Builders<TFile>.Update.Set(f => f.DeleteOn, file.DeleteOn);
 
             var result = (await Task.WhenAll(
                 this._formCollection.UpdateOneAsync(
@@ -69,7 +95,19 @@ namespace Demeter.FileComponent
                 ),
                 Task.Run(async () =>
                 {
-                    await DemeterFileUtil.DeleteAsync(this._baseFolderPath, form.Id);
+                    await DemeterFileUtil.DeleteAsync(this._baseFolderPath, file.Id);
+                    return UpdateResult.Unacknowledged.Instance as UpdateResult;
+                }),
+                Task.Run(async () => {
+                    if (this._elasticClient == null)
+                    {
+                        return UpdateResult.Unacknowledged.Instance as UpdateResult;
+                    }
+                    
+                    await this._elasticClient.DeleteAsync<TFile>(
+                        DocumentPath<TFile>.Id(file.Id)
+                    );
+
                     return UpdateResult.Unacknowledged.Instance as UpdateResult;
                 })
             ).ConfigureAwait(false))[0];
@@ -118,38 +156,87 @@ namespace Demeter.FileComponent
             }
         }
 
-        Task<IEnumerable<TFile>> IFormStore<TFile>.LastestAsync(int count, CancellationToken cancellationToken)
+       async Task<IEnumerable<TFile>> IFormStore<TFile>.LastestAsync(int count, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var query = Builders<TFile>.Filter.Eq(f => f.DeleteOn, null);
+
+            return await this._formCollection
+                .Find(query)
+                .SortByDescending(f => f.CreateOn)
+                .Limit(count)
+                .ToListAsync(cancellationToken)
+                .ConfigureAwait(false);
         }
 
-        Task<IEnumerable<TFile>> IFormStore<TFile>.QueryAsync(string queryString, int count, CancellationToken cancellationToken)
+        async Task<IEnumerable<TFile>> IFormStore<TFile>.QueryAsync(string queryString, int count, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
-        }
-
-        async Task<FormResult> IFormStore<TFile>.UpdateAsync(TFile form, CancellationToken cancellationToken)
-        {
-            if (form == null)
+            if (queryString == null)
             {
-                throw new ArgumentNullException(nameof(form));
+                throw new ArgumentNullException(nameof(queryString));
+            }
+            if (this._elasticClient == null)
+            {
+                throw new NullReferenceException(nameof(this._elasticClient));
+            }
+
+            var response = await this._elasticClient.SearchAsync<TFile>(s => s
+                .From(0).Size(count).Query(q =>
+                q.QueryString(m => m.Query(queryString))
+            ));
+
+            return response.Documents;
+        }
+
+        async Task<FormResult> IFormStore<TFile>.UpdateAsync(TFile file, CancellationToken cancellationToken)
+        {
+            if (file == null)
+            {
+                throw new ArgumentNullException(nameof(file));
             }
 
             var query = Builders<TFile>.Filter.And(
-                Builders<TFile>.Filter.Eq(f => f.Id, form.Id),
+                Builders<TFile>.Filter.Eq(f => f.Id, file.Id),
                 Builders<TFile>.Filter.Eq(f => f.DeleteOn, null)
             );
 
+            Mutex writeMutex = new Mutex(true);
             await Task.WhenAll(
-                DemeterFileUtil.WriteAsync(this._baseFolderPath, form.Id, form.Content),
+                Task.Run(async () =>
+                {
+                    writeMutex.WaitOne();
+                    await DemeterFileUtil.WriteAsync(this._baseFolderPath, file.Id, file.Content);
+                    writeMutex.ReleaseMutex();
+                }),
                 this._formCollection
                     .ReplaceOneAsync(
                         query,
-                        form,
+                        file,
                         new UpdateOptions { IsUpsert = false },
                         cancellationToken
-                )
+                ),
+                Task.Run(async () =>
+                {
+                    if (this._elasticClient == null)
+                    {
+                        return (ReplaceOneResult)ReplaceOneResult.Unacknowledged.Instance;
+                    }
+
+                    byte[] tempStore = file.Content;
+
+                    writeMutex.WaitOne();
+                    file.Content = null;
+                    await this._elasticClient.UpdateAsync<TFile>(
+                        DocumentPath<TFile>.Id(file.Id),
+                        update => update.Doc(file)
+                    );
+                    file.Content = tempStore;
+                    writeMutex.ReleaseMutex();
+
+                    return (ReplaceOneResult)ReplaceOneResult.Unacknowledged.Instance;
+                })
             ).ConfigureAwait(false);
+
+            writeMutex.Dispose();
 
             return FormResult.Success;
         }
